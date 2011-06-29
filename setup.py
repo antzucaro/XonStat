43e9e37ac0aeb9c88dc5c89ba67abfe84df03a0e
Ant Zucaro <azucaro@gmail.com>
2011-06-28 21:26:23 -0400

    Add dependencies

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ requires = [
     'repoze.tm2>=1.0b1', # default_commit_veto
     'zope.sqlalchemy',
     'WebError',
+    'pyramid-jinja2',
+    'sqlahelper',
+    'webhelpers',
+    'psycopg2',
     ]
 
 if sys.version_info[:3] < (2,5,0):
