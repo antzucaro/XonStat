78e172fb9d7f3493605a851a4f5f49c0f34d42e3
Ant Zucaro <azucaro@gmail.com>
2012-09-27 22:38:42 -0400

    Determine overall best rank by percentile, not the rank value.

--- xonstat/views/player.py
+++ xonstat/views/player.py
@@ -346,10 +346,14 @@ def get_ranks(player_id):
         rank = Rank(rank=row.rank,
             max_rank=row.max_rank,
             game_type_cd=row.game_type_cd)
-        
+
+        percentile = float(row.rank)/row.max_rank
+
         if not found_top_rank:
             ranks['overall'] = rank
             found_top_rank = True
+        elif percentile < float(ranks['overall'].rank)/ranks['overall'].max_rank:
+            ranks['overall'] = rank
 
         ranks[row.game_type_cd] = rank
 
