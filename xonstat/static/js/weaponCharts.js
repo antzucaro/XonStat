861fe3d7736d9d1a302791f9099f84c05c00a4a4
Ant Zucaro <azucaro@gmail.com>
2013-09-07 19:13:32 -0400

    Use the same colors for weapons each time.

--- xonstat/static/js/weaponCharts.js
+++ xonstat/static/js/weaponCharts.js
@@ -1,50 +1,54 @@
-    var drawDamageChart = function(data) {
-      // the chart should fill the "damageChart" div
-      var width = document.getElementById("damageChart").offsetWidth;
+var weapons = ["laser", "shotgun", "uzi", "grenadelauncher", "electro", "crylink",
+               "nex", "hagar", "rocketlauncher", "minstanex", "rifle",  "fireball",
+               "minelayer", "seeker", "tuba", "hlac", "hook", "porto"];
 
-      // transform the dataset into something nvd3 can use
-      var transformedData = d3.nest()
-        .key(function(d) { return d.weapon_cd; }).entries(data.weapon_stats);
+var drawDamageChart = function(data) {
+  // the chart should fill the "damageChart" div
+  var width = document.getElementById("damageChart").offsetWidth;
 
-      // transform games list into a map such that games[game_id] = linear sequence
-      var games = {};
-      data.games.forEach(function(v,i){ games[v] = i; });
+  // transform the dataset into something nvd3 can use
+  var transformedData = d3.nest()
+    .key(function(d) { return d.weapon_cd; }).entries(data.weapon_stats);
 
-      // margin model
-      var margin = {top: 20, right: 30, bottom: 30, left: 40},
-          height = 500 - margin.top - margin.bottom;
+  // transform games list into a map such that games[game_id] = linear sequence
+  var games = {};
+  data.games.forEach(function(v,i){ games[v] = i; });
 
-      width -= margin.left - margin.right;
+  // margin model
+  var margin = {top: 20, right: 30, bottom: 30, left: 40},
+      height = 500 - margin.top - margin.bottom;
 
-      // colors
-      var colors = d3.scale.category20();
-      keyColor = function(d, i) {return colors(d.key)};
+  width -= margin.left - margin.right;
 
-      var chart;
-      nv.addGraph(function() {
-        chart = nv.models.stackedAreaChart()
-          .margin(margin)
-          .width(width)
-          .height(height)
-          .x(function(d) { return games[d.game_id] })
-          .y(function(d) { return d.actual })
-          .color(keyColor);
+  // colors
+  var colors = d3.scale.category20().domain(weapons);
+  keyColor = function(d, i) {return colors(d.key)};
 
-        chart.xAxis
-          .axisLabel("Game ID")
-          .showMaxMin(false)
-          .ticks(5)
-          .tickFormat(function(d) { return data.games[d]; });
+  var chart;
+  nv.addGraph(function() {
+    chart = nv.models.stackedAreaChart()
+      .margin(margin)
+      .width(width)
+      .height(height)
+      .x(function(d) { return games[d.game_id] })
+      .y(function(d) { return d.actual })
+      .color(keyColor);
 
-        chart.yAxis
-          .tickFormat(d3.format(',02d'));
+    chart.xAxis
+      .axisLabel("Game ID")
+      .showMaxMin(false)
+      .ticks(5)
+      .tickFormat(function(d) { return data.games[d]; });
 
-        d3.select('#damageChartSVG')
-          .datum(transformedData)
-          .transition().duration(500).call(chart);
+    chart.yAxis
+      .tickFormat(d3.format(',02d'));
 
-        nv.utils.windowResize(chart.update);
+    d3.select('#damageChartSVG')
+      .datum(transformedData)
+      .transition().duration(500).call(chart);
 
-        return chart;
-      });
-    }
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+}
