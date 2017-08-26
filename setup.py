52f61795325be33b6b39df2c9f19f7f611074e9d
Ant Zucaro <azucaro@gmail.com>
2017-08-26 10:37:46 -0400

    Simplify the setup.py file.

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-import os
-import sys
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
@@ -20,34 +13,26 @@ requires = [
     'pyramid_beaker',
     'pyramid_mako',
     'waitress',
-    ]
+]
 
-if sys.version_info[:3] < (2,5,0):
-    requires.append('pysqlite')
-
-setup(name='XonStat',
-      version='0.0',
-      description='XonStat',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
+setup(
+    name='XonStat',
+    version='1.0',
+    description='XonStat',
+    long_description='XonStat: The statistics web application for Xonotic',
+    classifiers=[
         "Programming Language :: Python",
-        "Framework :: Pylons",
+        "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='xonstat',
-      install_requires = requires,
-      entry_points = """\
-      [paste.app_factory]
-      main = xonstat:main
-      """,
-      paster_plugins=['pyramid'],
-      )
+    ],
+    author='Ant Zucaro',
+    author_email='azucaro@gmail.com',
+    url='http://stats.xonotic.org',
+    keywords='web wsgi pylons pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires
+)
 
